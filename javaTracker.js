29b194dab761132de84386ae1ad6b1d7a3566104
Zara <zeeamjadtx@gmail.com>
2025-06-18 13:21:51 -0500

    add all files to github

---
+++ javaTracker.js
@@ -0,0 +1,9 @@
+// your JavaScript file
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+container.appendChild(content);
+
